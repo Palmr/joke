d77daa9262ac7d924dbbb689962539d22ed85c1a
Nick Palmer <nick@palmr.co.uk>
2026-05-11 09:45:24 +0100

    Simplify resetBuffer to a single clear() call, removing redundant limit and compact calls

--- src/main/java/uk/co/palmr/joke/KdbClient.java
+++ src/main/java/uk/co/palmr/joke/KdbClient.java
@@ -170,8 +170,6 @@ public class KdbClient implements AutoCloseable {
   }
 
   private void resetBuffer() {
-    messageBuffer.limit(messageBuffer.capacity());
-    messageBuffer.compact();
     messageBuffer.clear();
   }
 
